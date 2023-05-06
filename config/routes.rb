5fb4516b8b361755a37507cd77824e4611d2ebf7
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-05-06 12:28:05 +0000

    デバイス導入

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,16 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+
+devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+end
+
+devise_scope :admin do
+    get '/admin/sign_out' => 'admin/sessions#destroy'
+end
+
+root to: "public/homes#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
